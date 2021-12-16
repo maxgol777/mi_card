d357640c1b5dcca26c61d54cddb7cf81156c4b9e
Maxgol <maxgol777@gmail.com>
2021-12-16 12:59:08 +0200

    Add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,18 @@ class MyApp extends StatelessWidget {
               Text(
                 'Vasia Pupkin',
                 style: TextStyle(
+                    fontFamily: 'Pacifico',
                     fontSize: 40.0,
                     color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Flutter developer'.toUpperCase(),
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
                     fontWeight: FontWeight.bold),
               )
             ],
